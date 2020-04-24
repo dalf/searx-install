12da9caa333f0f12f11b0ae29e5913f93ccfa4de
Dalf <alex@al-f.net>
2020-04-24 10:47:05 +0200

    run.sh: Some comments

--- run.sh
+++ run.sh
@@ -1,12 +1,35 @@
 #!/bin/sh
 set -e
 
+VAGRANT=$(which vagrant)
+
+if [ -z ${VAGRANT} ]; then
+    echo "vagrant not found"
+    exit 1
+fi
+
+# create a patch (applied later in /shared/test/setup.sh )
 cd searx
 git diff > ../shared/searx.patch
-vagrant destroy
-vagrant up
-vagrant ssh -c 'sudo /shared/test/presetup.sh'
-vagrant reload
-vagrant ssh -c 'sudo /shared/test/setup.sh'
-vagrant reload
-vagrant ssh -c 'sudo /shared/test/check.sh'
+cd ..
+
+# destroy existing
+${VAGRANT} destroy -f
+
+# start
+${VAGRANT} up --parallel
+
+# system upgrade
+${VAGRANT} ssh -c 'sudo /shared/test/presetup.sh'
+
+# restart to apply all modifications
+${VAGRANT} reload
+
+# install searx
+${VAGRANT} ssh -c 'sudo /shared/test/setup.sh'
+
+# restart : make sure reboot is not a problem
+${VAGRANT} reload
+
+# check
+${VAGRANT} ssh -c 'sudo /shared/test/check.sh'
