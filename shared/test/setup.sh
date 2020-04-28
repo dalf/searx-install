3b142e2a9dfae75d4ba03fe41dd14d819bc2663f
Markus Heiser <markus.heiser@darmarit.de>
2020-04-28 14:12:21 +0200

    [brand] set SEARX_URL to VM's *host-only* IP
    BTW: with SEARX_URL, patch 234ade5 (setting PUBLIC_URL_MORTY) is no longer
    needed.
    
    See comment: https://github.com/asciimoo/searx/pull/1803#issuecomment-618483096
    
      Usually you set your SEARX_URL. Here in the automated tests, IPs are better
      because the containers do not have an entry in the DNS.
    
      If you change the SEARX_URL the scripts are using it .. if you leave SEARX_URL
      at the default value, the scripts are using IPs .. see
      https://github.com/return42/searx/blob/filtron/.config.sh#L20
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- shared/test/setup.sh
+++ shared/test/setup.sh
@@ -11,7 +11,12 @@ git clone https://github.com/return42/searx/
 cd searx
 git checkout filtron
 git apply /shared/searx.patch
+
+# brand: branch filtron
 sed -i 's/export GIT_BRANCH=master/export GIT_BRANCH=filtron/g' Makefile
+# brand: set SEARX_URL to VM's *host-only* IP
+sed -i "s/export SEARX_URL=https:\/\/searx.me/export SEARX_URL=http:\/\/$(/shared/test/eth1_ipv4.sh)\/searx/g" Makefile
+
 make buildenv
 
 chown -R vagrant:vagrant /srv/searx
@@ -19,7 +24,7 @@ chown -R vagrant:vagrant /srv/searx
 export FORCE_TIMEOUT=0
 ./utils/searx.sh install all
 ./utils/filtron.sh install all
-PUBLIC_URL_MORTY="http://$(/shared/test/eth1_ipv4.sh)/morty/" ./utils/morty.sh install all
+./utils/morty.sh install all
 
 ./utils/filtron.sh nginx install
 ./utils/morty.sh nginx install
