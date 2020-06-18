c8b9ed33368e025982b3b41225d45d8c6aa67890
Alexandre Flament <alex@al-f.net>
2020-06-18 16:07:59 +0200

    Generate the different configurations (apache|nginx)*(ubuntu|arch|fedora)

--- shared/test/check.sh
+++ shared/test/check.sh
@@ -7,4 +7,20 @@ echo "searx (filtron) URL:  http://$(/shared/test/eth1_ipv4.sh)/searx"
 echo "morty public URL:     http://$(/shared/test/eth1_ipv4.sh)/morty"
 echo
 
+# TODO
 # check systemd services
+# check localhost:8888 is opened
+# check localhost:4004 is opened
+# check localhost:4005 is opened
+# check localhost:3000 is opened
+# check 0.0.0.0:8888 is not opened
+# check 0.0.0.0:4004 is not opened
+# check 0.0.0.0:4005 is not opened
+# check 0.0.0.0:3000 is not opened
+# check http://localhost/searx answer
+# check as curl request to http://localhost/searx?q=test
+# check as firefox request to http://localhost/searx?q=test
+# check as firefox request to http://localhost/searx?q=!image+test
+# check if images are proxied
+# check each images
+# check filtron: 10 requests in one second --> check 429
--- shared/test/presetup.sh
+++ shared/test/presetup.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8; mode: bash -*-
 
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get -yq upgrade
+# ubuntu, debian, arch, fedora ...
+DIST_ID=$(source /etc/os-release; echo "$ID");
+
+case $DIST_ID in
+    ubuntu-*|debian-*)
+	export DEBIAN_FRONTEND=noninteractive
+	export DEBIAN_FRONTEND=noninteractive
+	apt-get update -y
+	apt-get upgrade -y
+	;;
+    arch)
+	pacman -Syu --noconfirm
+	;;
+    fedora)
+	dnf update -y
+	;;
+esac
