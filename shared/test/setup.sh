041e59087a0050ef9f865637d6bba24211afc574
Alexandre Flament <alex@al-f.net>
2020-06-18 19:50:31 +0200

    Use github.com/asciimoo/searx
    https://github.com/asciimoo/searx/pull/1803 was merged.

--- shared/test/setup.sh
+++ shared/test/setup.sh
@@ -9,13 +9,9 @@ HTTP_SERVER = "$1"
 mkdir -p /srv/
 cd /srv
 
-git clone https://github.com/return42/searx/
+git clone https://github.com/asciimoo/searx/
 cd searx
-git checkout filtron
-git apply /shared/searx.patch
 
-# brand: branch filtron
-sed -i 's/export GIT_BRANCH=master/export GIT_BRANCH=filtron/g' Makefile
 # brand: set SEARX_URL to VM's *host-only* IP
 sed -i "s/export SEARX_URL=https:\/\/searx.me/export SEARX_URL=http:\/\/$(/shared/test/eth1_ipv4.sh)\/searx/g" Makefile
 
